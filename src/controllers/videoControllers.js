d8f7eb4036f90d31fff976e9a7677ff21a52f5fd
hellicopthecat <114468016+hellicopthecat@users.noreply.github.com>
2023-02-09 20:43:54 +0900

    search

--- src/controllers/videoControllers.js
+++ src/controllers/videoControllers.js
@@ -1,7 +1,7 @@
 import VideoModel from "../models/videomodels";
 
 export const home = async (req, res) => {
-  const videos = await VideoModel.find({}).sort({createAt: "desc"});
+  const videos = await VideoModel.find({}).sort({createdAt: "desc"});
   return res.render("home", {pageTitle: "Home", videos});
 };
 export const watch = async (req, res) => {
@@ -80,6 +80,22 @@ export const postDelete = async (req, res) => {
   return res.redirect("/");
 };
 
-export const search = (req, res) => {
-  res.render("search", {pageTitle: "SEARCH"});
+export const search = async (req, res) => {
+  //console.log(req.query);
+  //req.query는 해당 html method get을 통한 주소창의 get값을 가져온다.
+  const {keyword} = req.query;
+  let videofind = [];
+  if (keyword) {
+    //if문에서 const를 사용시 if문 밖으로 도출 될 수 없기에 let문으로 업데이트를 할 수 있도록 만들어준다.
+    videofind = await VideoModel.find({
+      // regular expression 정규표현식을 이용하여 한글자만 쳐도 검색할 수 있게 만든다.
+      // i는 대소문자를 무시 $는 ***$사용시 마지막 ***단어로 검색하고 싶을 때 사용 ^***은 처음 단어
+      // 위를 사용하기 위해서는 regex라는 연산자(operator)를 사용.
+      title: {
+        $regex: new RegExp(keyword, "i"),
+        // 이는 mongoose가 아닌 mongo db에서 한다.
+      },
+    });
+  }
+  res.render("search", {pageTitle: "SEARCH", videofind});
 };
