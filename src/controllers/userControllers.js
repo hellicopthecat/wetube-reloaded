e8593b1505f30ea2757fcc6d1eabee99f49c4f01
hellicopthecat <114468016+hellicopthecat@users.noreply.github.com>
2023-02-10 13:49:04 +0900

    bcrypting password

--- src/controllers/userControllers.js
+++ src/controllers/userControllers.js
@@ -20,7 +20,10 @@ export const postJoin = async (req, res) => {
     name,
     location,
     // user를 저장하게 됨과 동시에 db에 password가 노출됨. 이에 따라 password를 hashing을 해줘야함.
-    // hashing은 일방향 함수, 문자열이 필요. 이번 실습에 사용될 것은 bcrypt. npm i bcrypt
+    // hashing은 일방향 함수, 문자열이 필요. 이번 실습에 사용될 것은 bcrypt. -- npm i bcrypt
+    // 해커가 해싱된 password를 가지고 할 수 있는 공격이 있는데 이것을 rainbow table이라고 한다.
+    // 이 rainbow table을 bcrypt가 막아줄 것이다.
+    // npmjs의 bcrypt 문서 tech2 참조 , saltRounds는 pw를 더 예측하기 어렵게 만듬
   });
   return res.redirect("/login");
 };
--- src/models/usermodel.js
+++ src/models/usermodel.js
@@ -1,4 +1,5 @@
 import mongoose from "mongoose";
+import bcrypt from "bcrypt";
 
 const userSchema = new mongoose.Schema({
   username: {type: String, minlength: 4, required: true, unique: true},
@@ -8,6 +9,14 @@ const userSchema = new mongoose.Schema({
   name: {type: String, required: true},
   location: String,
 });
+
+userSchema.pre("save", async function () {
+  this.password = await bcrypt.hash(this.password, 5);
+});
+// 먼저 bcrypt를 import 해준다.
+// userScheme 를 save 하기 전(pre) async function()을 할 건데 { 그것은
+// 이 (this). password는 해쉬를 할건데 그것은 이(this).password이며  saltRound를 5번 할것이고
+// 콜백 함수는 async await를 사용했기 때문에 쓸 필요가 없다. (bcrypt문서 async await promise 참조)}
 
 const UserModel = mongoose.model("user", userSchema);
 
